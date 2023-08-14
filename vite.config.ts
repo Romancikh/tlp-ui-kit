4b43e2a56ae8485f32899e13181f112dea3130a2
Kutimskii Roman <kutimskiy.roma@gmail.com>
2023-08-14 09:46:49 +0300

    Add Vite config

---
+++ vite.config.ts
@@ -0,0 +1,27 @@
+import { resolve } from "path";
+import { defineConfig } from "vite";
+import dts from "vite-plugin-dts";
+
+export default defineConfig({
+  plugins: [
+    dts({
+      insertTypesEntry: true,
+    }),
+  ],
+  build: {
+    lib: {
+      entry: resolve(__dirname, "src/index.ts"),
+      name: "TLP-UI-kit",
+    },
+    rollupOptions: {
+      external: ["react", "react-dom", "react/jsx-runtime"],
+      output: {
+        globals: {
+          react: "React",
+          "react/jsx-runtime": "jsxRuntime",
+          "react-dom": "ReactDOM",
+        },
+      },
+    },
+  },
+});
