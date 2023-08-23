849eca94dc8cabfbd667c0d4b6fddb9b5fa0e3f3
Kutimskii Roman <kutimskiy.roma@gmail.com>
2023-08-23 21:49:34 +0300

    Add TableRow

---
+++ src/TableRow/TableRow.tsx
@@ -0,0 +1,10 @@
+import MuiTableRow from "@mui/material/TableRow";
+import { PropsWithChildren } from "react";
+
+type TableRowProps = PropsWithChildren & {};
+
+function TableRow({ children }: TableRowProps) {
+  return <MuiTableRow>{children}</MuiTableRow>;
+}
+
+export default TableRow;
