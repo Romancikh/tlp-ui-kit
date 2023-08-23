e1b76ba4ef382b5455845f0aee116e2439c9b968
Kutimskii Roman <kutimskiy.roma@gmail.com>
2023-08-23 21:49:00 +0300

    Add TableHead

---
+++ src/TableHead/TableHead.tsx
@@ -0,0 +1,10 @@
+import MuiTableHead from "@mui/material/TableHead";
+import { PropsWithChildren } from "react";
+
+type TableHeadProps = PropsWithChildren & {};
+
+function TableHead({ children }: TableHeadProps) {
+  return <MuiTableHead>{children}</MuiTableHead>;
+}
+
+export default TableHead;
