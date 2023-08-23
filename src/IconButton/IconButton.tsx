1575ca1760df76bf76c75bf4da019c9de30d87c0
Kutimskii Roman <kutimskiy.roma@gmail.com>
2023-08-23 21:25:37 +0300

    Add IconButton

---
+++ src/IconButton/IconButton.tsx
@@ -0,0 +1,12 @@
+import MuiIconButton from "@mui/material/IconButton";
+import { PropsWithChildren } from "react";
+
+type IconButtonProps = PropsWithChildren & {
+  onClick: () => void;
+};
+
+function IconButton({ onClick, children }: IconButtonProps) {
+  return <MuiIconButton onClick={onClick}>{children}</MuiIconButton>;
+}
+
+export default IconButton;
